081a4a5d939023aa9fcd73719921dcd109ea7110
jhebert85 <jhebert85@gmail.com>
2022-06-10 19:48:47 -0500

    manager component fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,9 @@ import { EmployeeHeaderComponent } from './employee/employee-header/employee-hea
 import { EmployeeViewReimbComponent } from './employee/employee-view-reimb/employee-view-reimb.component';
 import { EmployeeCreateReimbComponent } from './employee/employee-create-reimb/employee-create-reimb.component';
 import { ManagerViewEmployeesComponent } from './manager/manager-view-employees/manager-view-employees.component';
-import { ManagerViewPendingReimbComponent } from './manager/manager-view-pending-reimb/manager-view-pending-reimb.component';
 import { ManagerHeaderComponent } from './manager/manager-header/manager-header.component';
+import { LoginComponent } from './user-login/login/login.component';
+import { ManagerViewReimbComponent } from './manager/manager-view-reimb/manager-view-reimb.component';
 
 @NgModule({
   declarations: [
@@ -19,8 +20,9 @@ import { ManagerHeaderComponent } from './manager/manager-header/manager-header.
     EmployeeViewReimbComponent,
     EmployeeCreateReimbComponent,
     ManagerViewEmployeesComponent,
-    ManagerViewPendingReimbComponent,
-    ManagerHeaderComponent
+    ManagerViewReimbComponent,
+    ManagerHeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
