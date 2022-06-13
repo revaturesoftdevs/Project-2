0dc9b21a37bb74379b1a0a64b8150a1763be3e6b
Goldendeep-kaur <kaurgoldendeep@gmail.com>
2022-06-13 19:01:57 -0400

    myWork

--- src/app/employee/employee-profile/employee-profile.component.ts
+++ src/app/employee/employee-profile/employee-profile.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Reimbursement } from '../employee-view-reimb/reimbursement.model';
 
 @Component({
-  selector: 'app-employee-profile',
+  selector: 'employee-profile',
   templateUrl: './employee-profile.component.html',
   styleUrls: ['./employee-profile.component.css']
 })
