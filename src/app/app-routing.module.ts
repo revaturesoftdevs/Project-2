e5e5f8acad22a124bdc995a99dc9dc2e250c4abc
berlyPaul360 <91963846+berlyPaul360@users.noreply.github.com>
2022-06-13 16:35:46 -0400

    navbar
    navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { MainLoginComponent } from './login/main-login/main-login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeViewReimbComponent } from './employee/employee-view-reimb/employee-view-reimb.component';
 
-const routes: Routes = [];
+import { ManagerViewEmployeesComponent } from './manager/manager-view-employees/manager-view-employees.component';
+
+const routes: Routes = [{path:"manager-view",component: ManagerViewEmployeesComponent},
+{path:"manager-view",component: ManagerViewEmployeesComponent},
+{path:"employee-view-reimb",component: EmployeeViewReimbComponent },
+{path: "main-login",component:MainLoginComponent}
+
+
+              ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ManagerServiceService } from './manager/manager-service.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -10,7 +11,11 @@ import { EmployeeCreateReimbComponent } from './employee/employee-create-reimb/e
 import { ManagerViewEmployeesComponent } from './manager/manager-view-employees/manager-view-employees.component';
 import { ManagerViewPendingReimbComponent } from './manager/manager-view-pending-reimb/manager-view-pending-reimb.component';
 import { ManagerHeaderComponent } from './manager/manager-header/manager-header.component';
+import {HttpClientModule} from '@angular/common/http';
 
+import { FormsModule } from '@angular/forms';
+import { EmployeeServiceService } from './employee/employee-service.service';
+import { MainLoginComponent } from './login/main-login/main-login.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,13 +25,17 @@ import { ManagerHeaderComponent } from './manager/manager-header/manager-header.
     EmployeeCreateReimbComponent,
     ManagerViewEmployeesComponent,
     ManagerViewPendingReimbComponent,
-    ManagerHeaderComponent
+    ManagerHeaderComponent,
+    MainLoginComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ManagerServiceService,EmployeeServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
